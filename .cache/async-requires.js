206b60f6a91112e5de381c9c056fc99c361bc07c
Denis <citofonare@denisrasia.com>
2023-02-25 18:22:29 +0100

    my lovely mess in git

---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---node-modules-gatsby-theme-andy-src-templates-note-js": () => import("./../../../node_modules/gatsby-theme-andy/src/templates/note.js?export=default" /* webpackChunkName: "component---node-modules-gatsby-theme-andy-src-templates-note-js" */)
+}
+
+
+
+exports.head = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
+  "component---node-modules-gatsby-theme-andy-src-templates-note-js": () => import("./../../../node_modules/gatsby-theme-andy/src/templates/note.js?export=head" /* webpackChunkName: "component---node-modules-gatsby-theme-andy-src-templates-note-jshead" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/denis/GitHub/notes/.cache/dev-404-page.js")),
+  "component---node-modules-gatsby-theme-andy-src-templates-note-js": preferDefault(require("/Users/denis/GitHub/notes/node_modules/gatsby-theme-andy/src/templates/note.js"))
+}
+
