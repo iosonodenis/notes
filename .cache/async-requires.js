806ef96251e77d28322ae17fb7b4306e149ab50e
Denis <citofonare@denisrasia.com>
2023-02-26 09:37:33 +0100

    cleanup

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,11 +1,4 @@
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---node-modules-gatsby-theme-andy-src-templates-note-js": () => import("./../../../node_modules/gatsby-theme-andy/src/templates/note.js?export=default" /* webpackChunkName: "component---node-modules-gatsby-theme-andy-src-templates-note-js" */)
-}
-
-
-
-exports.head = {
-
+  "component---node-modules-gatsby-theme-andy-src-templates-note-js": () => import("./../../../node_modules/gatsby-theme-andy/src/templates/note.js" /* webpackChunkName: "component---node-modules-gatsby-theme-andy-src-templates-note-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/denis/GitHub/notes/.cache/dev-404-page.js")),
   "component---node-modules-gatsby-theme-andy-src-templates-note-js": preferDefault(require("/Users/denis/GitHub/notes/node_modules/gatsby-theme-andy/src/templates/note.js"))
 }
 
